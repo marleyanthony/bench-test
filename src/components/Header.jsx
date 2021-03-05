8d8bf8dfc452c812d1666c43a3c9f9fc2355de69
Marley Anthony <62618323+marleyanthony@users.noreply.github.com>
2021-03-04 17:23:46 -0800

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <section className="header">
+      <h1 className="header-text">
+        Bench Test
+      </h1>
+    </section>
+  )
+}
+
+export default Header
