46955c303477ba0e6783a1818ff2f620220020d6
Marley Anthony <62618323+marleyanthony@users.noreply.github.com>
2021-03-04 19:33:53 -0800

    functionality for total amount

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,45 +3,76 @@ import axios from 'axios';
 
 const Table = () => {
   const [transactionData, setTransactionData] = useState([]);
+  const [totalAmount, setTotalAmount] = useState([]);
   const [pageNumber, setPageNumber] = useState(1);
+  const pageNumbers = [1, 2, 3, 4];
 
   useEffect(() => {
     axios
-      .get('https://resttest.bench.co/transactions/1.json')
+      .get(`https://resttest.bench.co/transactions/${pageNumber}.json`)
       .then((res) => {
-        // console.log(res.data.transactions)
         setTransactionData(res.data.transactions)
-        console.log(transactionData)
       });
-  }, [])
+  }, [pageNumber])
+
+  const pageNumberSelect = (e) => {
+    setPageNumber(e.target.innerHTML);
+  }
+
+  // ! this piece of code was obtained from stack overflow @ https://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript
+  const numberWithCommas = (x) => {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+
+  transactionData.map((item, index) => {
+    totalAmount.push(Math.abs(parseFloat(item.Amount, 10)))
+    console.log(totalAmount.reduce((a, b) => a + b, 0))
+  })
 
   return (
-    <section className="transaction-table">
-      <div className="transaction-table__labels">
-        <h2 className="transaction-table__label-name">Date</h2>
-        <h2 className="transaction-table__label-name">Company</h2>
-        <h2 className="transaction-table__label-name">Account</h2>
-        <h2 className="transaction-table__label-total">$39,664.53</h2>
-      </div>
-      <div className="transaction-table__info">
+    <>
+      <section className="transaction-table">
+        <div className="transaction-table__labels">
+          <h2 className="transaction-table__label-name">Date</h2>
+          <h2 className="transaction-table__label-name">Company</h2>
+          <h2 className="transaction-table__label-name">Account</h2>
+          <h2 className="transaction-table__label-total">
+            $ {
+              numberWithCommas(
+                totalAmount
+                  .reduce((a, b) => a + b, 0)
+                  .toFixed(2)
+              )
+            }
+          </h2>
+        </div>
+        <div className="transaction-table__info">
+          {
+            transactionData.map((transaction, index) => {
+              return (
+                <div className="transaction-table__info-row" key={index}>
+                  <h2 className="transaction-table__date">{transaction.Date}</h2>
+                  <h2 className="transaction-table__company">{transaction.Company}</h2>
+                  <h2 className="transaction-table__account">{transaction.Ledger}</h2>
+                  <h2 className="transaction-table__amount">$ {
+                    Math.abs(parseFloat(transaction.Amount, 10))
+                  }</h2>
+                </div>
+              )
+            })
+          }
+        </div>
+      </section>
+      <div className="transaction-table__page-select" onClick={pageNumberSelect}>
         {
-          transactionData.map((transaction, index) => {
+          pageNumbers.map((page, index) => {
             return (
-              <div className="transaction-table__info-row" key={index}>
-                <h2 className="transaction-table__date">{
-                  // Date.parse(transaction.Date)
-                  // new Date(transaction.Date) 
-                  transaction.Date
-                }</h2>
-                <h2 className="transaction-table__company">{transaction.Company}</h2>
-                <h2 className="transaction-table__account">{transaction.Ledger}</h2>
-                <h2 className="transaction-table__amount">$ {transaction.Amount}</h2>
-              </div>
+              <p className="transaction-table__page-number" key={index}>{page}</p>
             )
           })
         }
       </div>
-    </section>
+    </>
   )
 }
 
