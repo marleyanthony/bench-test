d1e0b6aacd9832b62eb49580f4d05c5e5eca150a
Marley Anthony <62618323+marleyanthony@users.noreply.github.com>
2021-03-04 17:29:36 -0800

    header component and styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <section className="header">
-      <h1 className="header-text">
+      <h1 className="header__text">
         Bench Test
       </h1>
     </section>
